2e136eec3b4eab68a2f95c588a9fcad0c3d00157
saitejamalyala <saiteja10294@gmail.com>
2021-08-11 10:50:10 +0200

    feat: added costmap creation notebook and costmap dataloader

--- config.py
+++ config.py
@@ -2,22 +2,23 @@
 from rosbag2numpy.losses import *
 
 params = {
-    "Description": "New data(only parking), coord conv, Normalized co-ordinates (paths, boundaries, ego/car odo, values range from 0-1536(grid min-grid max))",
-
+    #"Description": "Costmaps (distance and direction),New data(only parking), coord conv, Normalized co-ordinates (paths, boundaries, ego/car odo, values range from 0-1536(grid min-grid max))",
+    "Description": "Testing new loss",
     #-------------- Data set Parameters ------------#
     "H_BATCH_SIZE" :32,
     "H_SHUFFLE_BUFFER" : 32*100,
     "normalize_coords": True,
-    "normalize_factor": 1536,
+    "normalize_factor": 1.0,
     #"dataset_dir":'/netpool/work/gpu-3/users/malyalasa/New_folder/tf_records',
-    "dataset_dir":'/bigpool/projects/yao_SCANGAN360/New_Folder/tf_records',
+    #"dataset_dir":'/bigpool/projects/yao_SCANGAN360/New_Folder/tf_records',
+    "dataset_dir":'/bigpool/projects/yao_SCANGAN360/New_Folder/tf_records_w_costmap_dist_dir',
     #------------ Model Hyper paramaters ----------#
     "epochs":50,
     "lr":0.02,
-    "optimizer":'adam',
+    "optimizer":'nadam',
     "metric":'accuracy',#MeanAbsoluteError(),
-    "losses":[euclidean_distance_loss,endpoint_loss],
-    "loss_weights":[1,2],
+    "losses":[euclidean_distance_loss,endpoint_loss],#Using directly during model compilation file, not used
+    "loss_weights":[1,1,1],
     #----------- directory paths -----------------#
     "log_dir": '/netpool/work/gpu-3/users/malyalasa/New_folder/rosbag2numpy/logging',
 
