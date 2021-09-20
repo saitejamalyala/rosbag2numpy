d7053f66e6b5aaa7ac3377f0c7c4cc23777e1f0b
saiteja <saiteja10294@gmail.com>
2021-09-20 23:34:37 +0200

    add:feat_vector based data gen,pipeline, train

---
+++ data_processing/featdata_2_tfrec.py
@@ -0,0 +1,319 @@
+import numpy as np
+from numpy.core.fromnumeric import shape
+from numpy.lib.function_base import diff
+import tensorflow as tf
+from typing import List, Dict, Tuple, Type
+from numpy import ndarray
+from pathlib import Path
+from fnmatch import fnmatch
+import re
+import logging
+import os
+from tqdm import trange
+
+print(tf.__version__)
+
+# Logging setup
+logging.basicConfig(filename="tfrec_costmap_fv.log", filemode="a", level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(levelname)s- %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+class FvNpz_2_TfRec:
+    """Create tensorflow records from compressed numpy arrays"""
+
+    def __init__(self, source_path:Path, target_path:Path, samples_per_record: int = 16) -> None:
+        self.source_path = source_path
+        self.target_path = target_path
+        self.num_samples = samples_per_record  # samples per record
+
+        # file patterns to load data from
+        self.file_pattern = ['*_costmap_init_opt_diff_odo_fv.npz']
+
+        pass
+
+    #@staticmethod
+    def get_all_paths(self,root: Path, pattern: str)->List[Path]:
+        """Walk through root directory to find paths for file with requested file patterns
+
+        Args:
+            root (str): path to root directory
+            pattern (str): file pattern
+
+        Returns:
+            List[Path]: List of paths
+        """
+        list_file_paths = []
+        for path, subdirs, files in os.walk(root):
+            for file_name in files:
+                if fnmatch(file_name, pattern):
+                    list_file_paths.append((os.path.join(path, file_name)))
+
+        return sorted(list_file_paths)
+
+
+    @staticmethod
+    def __load_npz(
+        file_path: Path,
+        array_name: str,
+        data_type:Type =np.float32,
+        normalize_factor: float = 1.0,
+    ) -> ndarray:
+        """To load compressed numpy file and perform type casting anf normalization
+
+        Args:
+            file_path (str): Path to the compressed numpy array
+            array_name (str): array name used to create the numpy array
+            data_type ([type], optional): type of the data type. Defaults to np.float32.
+            normalize_factor (float, optional): Normalization factor. Defaults to 1.0.
+
+        Returns:
+            ndarray: loaded numpy array, normalized and type casted
+        """
+        npz_array = np.load(file_path)[array_name]
+        npz_array = (npz_array / normalize_factor).astype(data_type)
+
+        return npz_array
+
+    @staticmethod
+    def __get_names(path: Path) -> Tuple[str, str, str, str]:
+        """Get Scenario name, folder name, file name, file_prefix
+
+        Args:
+            path (str): os.path like path
+
+        Returns:
+            Tuple[str,str,str,str]: scene_name, folder_name, file_name, file_prefix
+        """
+        list_split_path = path.split(os.sep)
+        scene_name = list_split_path[-3]
+        folder_name = list_split_path[-2]
+        file_name = list_split_path[-1]
+        file_prefix = re.split("_nos", file_name)[0]
+
+        return scene_name, folder_name, file_name, file_prefix
+
+    @staticmethod
+    def __assert_lengths(in1, in2, in3, in4, in5, in6):
+        """[summary]
+
+        Args:
+            in1 ([type]): [description]
+            in2 ([type]): [description]
+            in3 ([type]): [description]
+            in4 ([type]): [description]
+            in5 ([type]): [description]
+            in6 ([type]): [description]
+            in7 ([type]): [description]
+        """
+        try:
+            #logger.info(f'{in1.shape[0]},{in2.shape[0]},{in3.shape[0]},{in4.shape[0]},{in5.shape[0]},{in6.shape[0]}')
+            assert (
+                len(in1)
+                == len(in2)
+                == len(in3)
+                == len(in3)
+                == len(in4)
+                == len(in5)
+                == len(in6)
+            )
+        except AssertionError:
+            print(f"Assertion error, all inputs and outputs are not of equal length")
+
+    @staticmethod
+    def __create_example(
+        costmap: ndarray,
+        init_path: ndarray,
+        feature_vector: ndarray,
+        opt_path: ndarray,
+        car_odo: ndarray,
+        unequal_indices: ndarray,
+        diff_path: ndarray,
+        file_details:str,
+    ) -> tf.train.Example:
+        """To create a tf.train.Example for sample
+
+        Args:
+            grid_map (ndarray): Occupancy grid map with shape(1536,1536)
+            grid_org_res (ndarray): Occupancy grid map origin and resolution-[x,y,resolution] with shape (,3)
+            left_bnd (ndarray): Left boundary with shape(25,2)
+            right_bnd (ndarray): Right boundary with shape (25,2)
+            car_odo (ndarray): Car centre co-ordinates and heading(orientation)-[x,y,theta] with shape (,3)
+            init_path (ndarray): Initial path with shape (25,2)
+            opt_path (ndarray): Optimal path with shape (25,2)
+
+        Returns:
+            tf.train.Example: Example with Features (Dict)
+        """
+        def gridmap_feature(value):
+            """Returns a bytes_list from a string / byte."""
+
+            return tf.train.Feature(
+                bytes_list=tf.train.BytesList(value=[value.tostring()])
+            )
+
+        def bytes_feature(value):
+            """Returns a bytes_list from a string / byte."""
+            return tf.train.Feature(
+                bytes_list=tf.train.BytesList(value=[value.tostring()])
+            )
+        
+        def string_feature(value):
+            """Returns a bytes_list from a string / byte."""
+            return tf.train.Feature(
+                bytes_list=tf.train.BytesList(value=[value])
+            )
+
+        feature = {
+            # model inputs
+            "cost_map": gridmap_feature(costmap),
+            "init_path": bytes_feature(init_path),
+            "feature_vector": bytes_feature(feature_vector),
+            "opt_path": bytes_feature(opt_path),
+            "car_odo": bytes_feature(car_odo),
+            "file_details":string_feature(file_details),
+            "unequal_indices":bytes_feature(unequal_indices),
+            # model outputs
+            "diff_path":bytes_feature(diff_path)
+        }
+        return tf.train.Example(features=tf.train.Features(feature=feature))
+
+    def __write_records(self, grid_path: Path, **file_data: Dict[str, ndarray]):
+        """Write the data in ndarrays to tf records
+
+        Args:
+            grid_path (Path): grid data path to extract scene name, folder name,
+             bag file name and file prefix(to be appened to tfrec file name).
+             Can use other path also instead of grid_path
+
+            file_data(Dict[str, ndarray]): Dictionary with names of the ndarrays as keys and ndarrays as values
+        """
+
+        costmap = file_data.get("ip_costmap")
+        init_path = file_data.get("ip_init_path")
+        feature_vector = file_data.get("ip_fv")
+        opt_path = file_data.get("ip_opt_path")
+        car_odo = file_data.get("ip_odo")
+        unequal_indices = file_data.get("ip_unequal_path_indices")
+        
+        diff_path = file_data.get("op_diff_path")
+
+
+        scene_name, folder_name, file_name, file_prefix = self.__get_names(grid_path)
+
+        current_tfrec_dir = os.path.join(self.target_path, scene_name, folder_name)
+
+        num_tfrecods = len(costmap) // self.num_samples
+
+        if len(costmap) % self.num_samples:
+            num_tfrecods += 1  # add one record if there are any remaining samples
+
+        logger.info(
+            f"file:{file_name} having {len(costmap)} samples, creating {num_tfrecods} records"
+        )
+
+        if not os.path.exists(current_tfrec_dir):
+            os.makedirs(current_tfrec_dir)
+
+        for tfrec_num in range(num_tfrecods):
+            samples = costmap[
+                (tfrec_num * self.num_samples) : ((tfrec_num + 1) * self.num_samples)
+            ]
+
+            with tf.io.TFRecordWriter(
+                f"{current_tfrec_dir}/{file_prefix}_file_{tfrec_num:02d}-{len(samples)}.tfrec"
+            ) as writer:
+                for s in range(len(samples)):
+                    # print(np.shape(np_init_path[i]))
+                    # create example for single sample
+                    # Order of the parameters passed in __create_example is critically important
+                    if np.array_equiv(init_path[s],np.zeros_like(init_path[s])):
+                        logger.info(f"Files with zeros: {scene_name}/{folder_name}/{file_name}")
+                    elif np.array_equiv(opt_path[s],np.zeros_like(opt_path[s])):
+                        logger.info(f"Files with zeros: {scene_name}/{folder_name}/{file_name}")
+                    elif np.array_equiv(car_odo[s],np.zeros_like(car_odo[s])):
+                        logger.info(f"Files with zeros: {scene_name}/{folder_name}/{file_name}")
+
+                    example = self.__create_example(
+                        costmap=costmap[s],
+                        init_path=init_path[s],
+                        feature_vector=feature_vector[s],
+                        car_odo=car_odo[s],
+                        opt_path=opt_path[s],
+                        unequal_indices = unequal_indices,
+
+                        diff_path = diff_path[s],
+                        file_details = bytes(f"{scene_name}/{folder_name}/{file_name}",encoding='utf-8')
+                    )
+                    # serialize the single sample example to string
+                    writer.write(example.SerializeToString())
+    
+
+    def create_records(self):
+
+
+        # get all paths
+        file_paths = self.get_all_paths(root=self.source_path, pattern=self.file_pattern[0])
+  
+        # iterate through every path
+        for f in trange(len(file_paths)):
+
+            #potential inputs
+            ip_costmap = self.__load_npz(file_path=file_paths[f],array_name='costmap')
+            #convert to uint8 to save disk space, divide by 255 when using
+            ip_costmap = (ip_costmap*255).astype(np.uint8)
+
+            ip_init_path = self.__load_npz(file_path=file_paths[f],array_name='init_path')
+            ip_opt_path = self.__load_npz(file_path=file_paths[f],array_name='opt_path')
+            ip_odo = self.__load_npz(file_path=file_paths[f],array_name='odo_data')
+            ip_fv = self.__load_npz(file_path=file_paths[f],array_name='feature_vector')
+            ip_unequal_path_indices = self.__load_npz(file_path=file_paths[f],array_name='unequal_path_idx')
+
+            #output
+            op_diff_path = self.__load_npz(file_path=file_paths[f],array_name='diff_path')
+
+            self.__assert_lengths(
+                ip_costmap,
+                ip_init_path,
+                ip_opt_path,
+                ip_odo,
+                ip_fv,
+                op_diff_path,
+            )
+
+
+        # load all data corresponding to that file path
+        # for every loaded data write records
+            dict_data = {
+                "ip_costmap":ip_costmap,
+                "ip_init_path":ip_init_path,
+                "ip_opt_path":ip_opt_path,
+                "ip_odo":ip_odo,
+                "ip_fv":ip_fv,
+                "ip_unequal_path_indices":ip_unequal_path_indices,
+                "op_diff_path":op_diff_path,
+            }
+
+            self.__write_records(file_paths[f], **dict_data)
+
+
+        pass
+
+
+
+if __name__ == "__main__":
+
+    s_path = r"C:\Users\Teja\Documents\_INFOTECH\Thesis\sample_Ros_bag\np_data\raw_data_wo_img\scenario1"
+    tgt_path = r"D:\tf_records_w_cm_fv_diff"
+    #r"C:\Users\Teja\Documents\_INFOTECH\Thesis\sample_Ros_bag\np_data\tfrec_fv_costmap"
+
+    tf_rec_maker = FvNpz_2_TfRec(
+        source_path=s_path, target_path=tgt_path, samples_per_record=16
+    )
+    #print(len(tf_rec_maker.get_all_paths(root=s_path,pattern='*_costmap_init_opt_diff_odo_fv.npz')))
+    tf_rec_maker.create_records()
+
---
+++ data_processing/gen_feat_data.py
@@ -0,0 +1,330 @@
+"""
+# To be used after extracting the data from rosbag to compressed numpy arrays
+
+# sample inputs required to run this script: path to folder inside scenario 
+with _grid.npz, _init_path.npz (real world co-ordinates), _opt_path.npz(real world co-ordinates), _odo.npz(real world co-ordinates).
+
+# output: single npz file consisting of costmap, feature vector, difference vector (init_path - opt_path), init_path, opt_path, car odo 
+"""
+
+from typing import List
+from numpy import ndarray
+import numpy as np
+from fnmatch import fnmatch
+import os
+from pathlib import Path
+from tqdm import trange
+
+class FeatGen:
+    """
+    To generate feature vector and save it in a compressed .npz format 
+    """
+    def __init__(self,x_off:int,y_off:int,root_path) -> None:
+        
+        self.root_path = root_path
+        self.file_pattern = '*_grid.npz'
+        self.x_offset = x_off
+        self.y_offset = y_off
+        pass
+
+    def get_all_grid_paths(self,print_path:bool=False)->List:
+        """Get all .npz files with "_grid.npz" pattern in the specified root directory
+
+        Args:
+            self.root_path : Root path where all your _grid.npz are located
+            self.file_pattern : pattern string to search
+            print_path (bool, optional): If true prints paths of all files found. Defaults to False.
+
+        Returns:
+            List: list containing all paths where _grid.npz (file pattern string) files are located.
+
+        """
+        list_gridmap_loc=[]
+        for path, subdirs, files in os.walk(self.root_path):
+            for name in files:
+                if fnmatch(name, self.file_pattern):
+                    if print_path:
+                        print(os.path.join(path, name))
+                    list_gridmap_loc.append(os.path.join(path,name))
+
+        return list_gridmap_loc
+
+    def transform_coord(self,path,grid_info):
+        assert grid_info.shape[0] == 3
+        if len(path.shape)==2:
+            assert path.shape[1]==2
+            org = grid_info[0:2]
+            res = grid_info[2]
+            return (path-org)/res
+        elif len(path.shape)==1:
+            assert path.shape[0]==2
+            org = grid_info[0:2]
+            res = grid_info[2]
+            return (path-org)/res
+
+    def load_np_data(self,grid_path:str,print_shapes:bool=True):
+        """load all numpy arrays (grid_data{categorical: 0,127}, _init_path, _opt_path, car_odo {all in real world coordinates})
+
+        Args:
+            grid_path (str): path to _grid.npz file
+
+        Returns:
+            multiple nd arrays: grid_data, grid_org_res, init_data, opt_data, odo_data
+        """
+        #0,127
+        test_grid_data = np.load(file=grid_path)['grid_data']
+        test_org_res = np.load(file=grid_path)['grid_org_res']
+        # real world coords
+        test_init_data = np.load(file=grid_path.replace('_grid','_init_path'))['init_path']
+        test_opt_data = np.load(file=grid_path.replace('_grid','_opt_path'))['opt_path']
+        test_odo_data = np.load(file=grid_path.replace('_grid','_odo'))['odo_data']
+
+        if print_shapes:
+            print(f"grid shape:{test_grid_data.shape},org_res:{test_org_res.shape},init_path:{test_init_data.shape}")
+
+        return test_grid_data,test_org_res,test_init_data,test_opt_data,test_odo_data
+
+    def calc_direction_cost(self,car_coords:ndarray,endpoint_unit_normal:ndarray,vector:ndarray)->ndarray:
+        """To calculate direction cost for each cell in occupancy gridmap w.r.t to car location and destination co-ordinate.
+            Calculates direction weight for the costmap. 
+            step 1: calculate direction vector for every co-ordinate 
+            step 2: normalize the vector calculated in step 1  
+
+        Args:
+            car_coords (ndarray): Car Current location
+            endpoint_unit_normal (ndarray): Unit normal vector between car location and ending point
+            vector (ndarray): specific co-ordinate
+
+        Returns:
+            [ndarray]: array of direction cost for that vector
+        """
+        direction = car_coords-vector
+        direction_unit = direction/np.linalg.norm(x=direction)
+
+        direction_cost = np.dot(direction_unit,endpoint_unit_normal)
+
+        return direction_cost
+
+    def calc_costmap(self,gridmap:ndarray,gd_org_res:ndarray,car_odo:ndarray,initial_path:ndarray,include_dir_cost:bool=True,distance_mag:float=0.23)->ndarray:
+        """Function to transfor grid map to cost map
+
+        Args:
+            gridmap (ndarray): Binary occupancy grid map (1536 * 1536)- data range : 0,1
+            car_odo (ndarray): Car location x,y,theta - transformed cordinate values (in range of 0-1536)
+            initial_path (ndarray): initial path proposed by trajectory planning module (in range of 0-1536)
+            distance_mag (float, optional): lower it is larger cost considered for the occupied cells. Defaults to 0.23.
+
+        Returns:
+            ndarray: Cost map  array
+        """
+        
+        car_coords = car_odo[0:2]
+        end_coords = initial_path[-1]
+        
+        # Normalize coordinates to 0-1536 range
+        #car_coords = (car_coords-gd_org_res[0:2])/gd_org_res[2]
+        #end_coords = (end_coords-gd_org_res[0:2])/gd_org_res[2]
+
+        # Initialize distance and direction costmap 
+        dist_costmap = np.zeros(shape=(1536,1536))
+        dir_costmap =  np.zeros(shape=(1536,1536))
+
+        ################# calculate distance costmap ################
+        
+        # get occuppied cell indices
+        (y,x) = np.where(gridmap==np.max(gridmap))
+        # create coordinate pairs of location of occupied cells
+        coords = np.array((x,y)).T
+
+        # eucledian difference
+        distance = np.square(coords-car_coords)
+        distance = np.sum(distance, axis=1)
+        distance = 1/(distance**distance_mag)
+        dist_costmap[(y,x)]=distance
+
+        ################# Direction cost map ######################
+        
+        if include_dir_cost:
+
+            # end point direction
+            direction_vector = car_coords-end_coords
+
+            # unit vector between, car location and end point
+            end_dir_unit = direction_vector/np.linalg.norm(x=direction_vector)
+
+            # calculate dir cost for all occupied cell locations
+            direction_cost= np.asarray([self.calc_direction_cost(car_coords=car_coords,endpoint_unit_normal=end_dir_unit,vector=coord) for coord in coords])
+
+            # zero down all opposite directions to end point
+            direction_cost[np.where(direction_cost<0)]=0.0
+
+            dir_costmap[(y,x)] = direction_cost
+        else:
+            dir_costmap = np.ones(shape=(1536,1536))
+
+        # pointwise multiply distance cost and direction cost
+        costmap = dist_costmap*dir_costmap
+
+        # normalize cost map between [0,1]
+        costmap = costmap/np.max(costmap)
+        #cp=r'C:\Users\Teja\Documents\_INFOTECH\Thesis\sample_Ros_bag\npzcostmaps'
+        #np.savez_compressed(os.path.join(cp,file_details.decode("utf-8").split('/')[-1]),grid_data=costmap.astype(np.float16))
+        return costmap.astype(float)
+
+    def get_feature_vector(self,gridmap:ndarray,init_path:ndarray,offset:List[int]=[13,20])->ndarray:
+        """Get feature vector from grodmap, initial path , offset(Tuple of x offset and y offset)
+
+        Args:
+            gridmap (ndarray): Costmap (Obtained after transformation from binary occupancy grid map)
+            init_path (ndarray): [description]
+            offset ([type]): [description]
+
+        Returns:
+            ndarray: Feature vector of shape (25,4*offset[0]*y_offset[1])
+        """
+        feature_vector = np.zeros(shape=(init_path.shape[0],4*offset[0]*offset[1]))
+        # np.array(init_path.shape[0],4*offset[0]*offset[1])
+        for i in range(init_path.shape[0]):
+            x_l,y_l = (init_path[i].astype(int)) - offset
+            x_h,y_h = (init_path[i].astype(int)) + offset
+            cell = 0
+            #print("lower:",(x_l,y_l))
+            #print("higher:",(x_h,y_h))
+
+            for x in range(x_l,x_h):
+                for y in range(y_l,y_h):
+                    if cell<feature_vector.shape[1]:
+                        #gridmap[row][column]
+                        feature_vector[i][cell] = gridmap[y][x]
+                        cell+=1
+        return feature_vector
+
+    def check_imbalance(self,init_path_arr:ndarray,opt_path_arr:ndarray)->List:
+        """To check at which indices in an array initial path and optimized path are different from each other
+
+        Args:
+            init_path_arr ([ndarray]): initial paths
+            opt_path_arr ([ndarray]): optimized paths
+
+        Returns:
+            List: List of indices
+        """
+        count=0
+        zero_paths = 0
+        indices = []
+        assert len(opt_path_arr)==len(init_path_arr)
+        for i in range(len(init_path_arr)):
+            if np.array_equal(init_path_arr[i],opt_path_arr[i]):
+                count +=1
+            if not np.array_equal(init_path_arr[i],opt_path_arr[i]):
+                indices.append(i)
+        return indices    
+                
+    def create_and_save_costmap_diff_featurevector(self,list_gridmap_loc:List):
+
+        # looping though all npz files
+
+        for i in trange(len(list_gridmap_loc)):
+
+            #i=0
+            target_location = os.path.split(list_gridmap_loc[i])[0]
+
+            np_grid_map,np_org_res,np_init_data,np_opt_data,np_odo_data = self.load_np_data(grid_path=list_gridmap_loc[i]) 
+
+            ### initialize lists to fold modified data
+            cost_maps = []
+            init_paths=[]
+            opt_paths=[]
+            car_odo_data=[]
+            diff_paths = []
+            list_features = []
+
+            print(f"number of samples : {np_grid_map.shape[0]}")
+
+            ################# get unequal path indices #############
+            """ Shape changes for every file , information about indices of samples that have opt path different from init path"""
+            unequal_indices = self.check_imbalance(np_init_data,np_opt_data)
+
+
+            # looping through all samples in loaded numpy file
+            for j in range(np_grid_map.shape[0]):
+
+                # to transform from real world coordinates to grid map co-ordinates
+                # grid coords(after tranformation)- (0-1536)
+                tf_np_init_data = self.transform_coord(np_init_data[j],np_org_res[j])
+                tf_np_opt_data = self.transform_coord(np_opt_data[j],np_org_res[j])
+                tf_np_odo_data = self.transform_coord(np_odo_data[j][0:2],np_org_res[j])
+
+
+                total_cost_map = self.calc_costmap(gridmap=np_grid_map[j]/127,gd_org_res=np_org_res[j],
+                                        car_odo=tf_np_odo_data,initial_path=tf_np_init_data,
+                                        include_dir_cost=True)
+
+                ###### Calculate feature vector from costmap and init_path ############
+                fv = self.get_feature_vector(gridmap=total_cost_map,init_path = tf_np_init_data,offset= [self.x_offset,self.y_offset])
+
+                assert fv.shape[0]==tf_np_init_data.shape[0]
+                
+                # append eventually, Very inefficient way *** !!! correct it later !!!
+                cost_maps.append(total_cost_map)
+                init_paths.append(tf_np_init_data)
+                opt_paths.append(tf_np_opt_data)
+                diff_paths.append(tf_np_init_data-tf_np_opt_data)
+                car_odo_data.append(tf_np_odo_data)
+                list_features.append(fv)
+
+
+            assert len(init_paths)==len(opt_paths)==len(diff_paths)==len(car_odo_data)==np_grid_map.shape[0]
+            
+            #print(len(init_paths))
+            ################ convert data to nd arrays to save in .npz #########################
+
+            # costmap data range: 0.0-1.0 
+            arr_np_cost_map = np.array(cost_maps)
+
+            # all co-ordinates date range - [0.0-1536.0] , in grid co-ordinates
+            arr_np_init_path = np.array(init_paths)
+            arr_np_opt_path = np.array(opt_paths)
+            arr_np_diff_path = np.array(diff_paths)
+            arr_np_odo = np.array(car_odo_data)
+
+            # list of features each of shape 25,4*x_offset*y_offset
+            arr_np_fv = np.array(list_features)
+
+            # list of indices- integers in list
+            arr_neq_indices = np.array(unequal_indices)
+
+            ############### save data in the same location as source path ##########################
+            np.savez_compressed(
+                list_gridmap_loc[i].replace("_grid","_costmap_init_opt_diff_odo_fv_all"),
+                costmap = arr_np_cost_map,
+                init_path = arr_np_init_path,
+                opt_path = arr_np_opt_path,
+                diff_path = arr_np_diff_path,
+                odo_data = arr_np_odo, 
+                feature_vector = arr_np_fv,
+                unequal_path_idx = arr_neq_indices,
+                )
+
+
+if __name__ == "__main__":
+
+    source_path = r'C:\Users\Teja\Documents\_INFOTECH\Thesis\sample_Ros_bag\np_data\raw_data_wo_img\scenario1'
+
+    feat = FeatGen(x_off=13,y_off=20, root_path=source_path)
+
+    #get all gridmap npz files
+    grid_paths = feat.get_all_grid_paths(print_path=True)
+
+    # load data, get all corresponding data in that respective folder for every gridmap npz
+    test_grid_data,test_org_res,test_init_data,test_opt_data,test_odo_data = feat.load_np_data(grid_path=grid_paths[0])
+
+    # print to test shapes of loaded data
+    print(test_grid_data.shape,test_org_res.shape,test_init_data.shape,test_opt_data.shape,test_odo_data.shape)
+
+    #check unequal indices functionality
+    print(f"Unequal Indices: {feat.check_imbalance(test_init_data,test_opt_data)}")
+
+    #saves new npz files in the same location as source path
+    feat.create_and_save_costmap_diff_featurevector(list_gridmap_loc=grid_paths)
+
